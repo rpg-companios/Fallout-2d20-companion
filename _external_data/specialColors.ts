23c310ad6f9ce2fdc0791f37dcbdaf3206da60bc
Владимир <handylinux@gmail.com>
2026-06-02 11:40:57 +0400

    Delete _external_data directory

--- _external_data/specialColors.ts
+++
@@ -1,9 +0,0 @@
-export const SPECIAL_COLORS: Record<string, string> = {
-  strength:     'var(--color-special-strength)',
-  perception:   'var(--color-special-perception)',
-  endurance:    'var(--color-special-endurance)',
-  charisma:     'var(--color-special-charisma)',
-  intelligence: 'var(--color-special-intelligence)',
-  agility:      'var(--color-special-agility)',
-  luck:         'var(--color-special-luck)',
-};
